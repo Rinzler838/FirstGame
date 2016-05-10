f42f64712c92bc587ee375af8e3acf15dc5fcec9
Rinzler838 <dvil4688@740s-pl-105.local>
2016-05-10 09:43:14 -0600

    Created new project in Xamarin

---
+++ Mac/Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppKit;
+
+namespace FirstGame.Mac
+{
+	static class MainClass
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+			NSApplication.Main (args);
+		}
+	}
+}
+
