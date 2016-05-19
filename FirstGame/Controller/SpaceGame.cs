29ea2be26c0e0a47894da963f3a468b8dd719c3d
Rinzler838 <dvil4688@740s-pl-109.local>
2016-05-19 07:24:44 -0600

    Added new backgrounds and Enemy code

--- FirstGame/Controller/SpaceGame.cs
+++ FirstGame/Controller/SpaceGame.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 using FirstGame.Model;
 using FirstGame.View;
 
@@ -37,6 +38,18 @@ namespace FirstGame.Controller
 		private ParallaxingBackground bgLayer1;
 		private ParallaxingBackground bgLayer2;
 
+		// Enemies
+		Texture2D enemyTexture;
+		List<Enemy> enemies;
+
+		// The rate at which the enemies appear
+		TimeSpan enemySpawnTime;
+		TimeSpan previousSpawnTime;
+
+		// A random number generator
+		Random random;
+
+
 		public SpaceGame ()
 		{
 			graphics = new GraphicsDeviceManager (this);
@@ -61,6 +74,18 @@ namespace FirstGame.Controller
 			//Initialize background layers
 			bgLayer1 = new ParallaxingBackground();
 			bgLayer2 = new ParallaxingBackground();
+
+			// Initialize the enemies list
+			enemies = new List<Enemy> ();
+
+			// Set the time keepers to zero
+			previousSpawnTime = TimeSpan.Zero;
+
+			// Used to determine how fast enemy respawns
+			enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+
+			// Initialize our random number generator
+			random = new Random();
             
 			base.Initialize ();
 		}
@@ -78,18 +103,20 @@ namespace FirstGame.Controller
 
 			// Load the player resources
 			Animation playerAnimation = new Animation();
-			Texture2D playerTexture = Content.Load<Texture2D>("Animation/ImportedMetroid");
-			playerAnimation.Initialize(playerTexture, Vector2.Zero, 78, 86, 20, 45, Color.White, 1f, true);
+			Texture2D playerTexture = Content.Load<Texture2D>("Animation/ImportedCharging");
+			playerAnimation.Initialize(playerTexture, Vector2.Zero, 36, 44, 20, 45, Color.White, 1f, true);
 
 			Vector2 playerPosition = new Vector2 (GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y
 				+ GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
 			player.Initialize(playerAnimation, playerPosition);
 
 			// Load the parallaxing background
-			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
-			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
+			bgLayer1.Initialize(Content, "Texture/ImportedBGOne", GraphicsDevice.Viewport.Width, -1);
+			bgLayer2.Initialize(Content, "Texture/ImportedBGTwo", GraphicsDevice.Viewport.Width, -2);
 
-			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
+			mainBackground = Content.Load<Texture2D>("Texture/ImportedStatic");
+
+			enemyTexture = Content.Load<Texture2D>("Animation/ImportedMetroid");
 		}
 
 		private void UpdatePlayer(GameTime gameTime)
@@ -111,16 +138,16 @@ namespace FirstGame.Controller
 			{
 				player.Position.X += playerMoveSpeed;
 			}
-			if (currentKeyboardState.IsKeyDown(Keys.Up) ||
-				currentGamePadState.DPad.Up == ButtonState.Pressed)
-			{
-				player.Position.Y -= playerMoveSpeed;
-			}
-			if (currentKeyboardState.IsKeyDown(Keys.Down) ||
-				currentGamePadState.DPad.Down == ButtonState.Pressed)
-			{
-				player.Position.Y += playerMoveSpeed;
-			}
+//			if (currentKeyboardState.IsKeyDown(Keys.Up) ||
+//				currentGamePadState.DPad.Up == ButtonState.Pressed)
+//			{
+//				player.Position.Y -= playerMoveSpeed;
+//			}
+//			if (currentKeyboardState.IsKeyDown(Keys.Down) ||
+//				currentGamePadState.DPad.Down == ButtonState.Pressed)
+//			{
+//				player.Position.Y += playerMoveSpeed;
+//			}
 
 			// Make sure that the player does not go out of bounds
 			player.Position.X = MathHelper.Clamp(player.Position.X, 0,GraphicsDevice.Viewport.Width - player.Width);
@@ -157,8 +184,99 @@ namespace FirstGame.Controller
 			// Update the parallaxing background
 			bgLayer1.Update();
 			bgLayer2.Update();
+
+			// Update the enemies
+			UpdateEnemies(gameTime);
+
+			// Update the collision
+			UpdateCollision();
             
 			base.Update (gameTime);
+		}
+
+		private void UpdateCollision()
+		{
+			// Use the Rectangle's built-in intersect function to 
+			// determine if two objects are overlapping
+			Rectangle rectangle1;
+			Rectangle rectangle2;
+
+			// Only create the rectangle once for the player
+			rectangle1 = new Rectangle((int)player.Position.X,
+				(int)player.Position.Y,
+				player.Width,
+				player.Height);
+
+			// Do the collision between the player and the enemies
+			for (int i = 0; i <enemies.Count; i++)
+			{
+				rectangle2 = new Rectangle((int)enemies[i].Position.X,
+					(int)enemies[i].Position.Y,
+					enemies[i].Width,
+					enemies[i].Height);
+
+				// Determine if the two objects collided with each
+				// other
+				if(rectangle1.Intersects(rectangle2))
+				{
+					// Subtract the health from the player based on
+					// the enemy damage
+					player.Health -= enemies[i].Damage;
+
+					// Since the enemy collided with the player
+					// destroy it
+					enemies[i].Health = 0;
+
+					// If the player health is less than zero we died
+					if (player.Health <= 0)
+						player.Active = false; 
+				}
+
+			}
+		}
+
+		private void AddEnemy()
+		{ 
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 78, 86, 20, 60,Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width +enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height -100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position); 
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+
+		private void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime) 
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--) 
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					enemies.RemoveAt(i);
+				} 
+			}
 		}
 
 		/// <summary>
@@ -178,6 +296,12 @@ namespace FirstGame.Controller
 			// Draw the moving background
 			bgLayer1.Draw(spriteBatch);
 			bgLayer2.Draw(spriteBatch);
+
+			// Draw the Enemies
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				enemies[i].Draw(spriteBatch);
+			}
 
 			// Draw the Player
 			player.Draw(spriteBatch);
